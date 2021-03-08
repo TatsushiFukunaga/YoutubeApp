f76477843cfacf1caaabaab1e26c2ac5de81a398
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-03-08 15:00:27 +0900

    動画画面のimageViewをドラッグで小さくする

--- YoutubeApp/ViewControllers/VideoViewController.swift
+++ YoutubeApp/ViewControllers/VideoViewController.swift
@@ -18,6 +18,9 @@ class VideoViewController: UIViewController {
     @IBOutlet weak var channelTitleLabel: UILabel!
     @IBOutlet weak var baseBackGroundView: UIView!
     @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var videoImageViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var videoImageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var videoImageViewTrailingConstraint: NSLayoutConstraint!
     
     
     override func viewDidLoad() {
@@ -63,19 +66,54 @@ class VideoViewController: UIViewController {
             
             imageView.transform = CGAffineTransform.init(translationX: 0, y: move.y)
             
+            // 左右のpadding設定
+            let movingConstant = move.y / 30
+            
+            if videoImageViewLeadingConstraint.constant <= 12 {
+                videoImageViewTrailingConstraint.constant = movingConstant
+                videoImageViewLeadingConstraint.constant = movingConstant
+            }
+            
+            // ImageViewの高さの動き
+            // 280(最大値) - 70(最小値) = 210
+            let pararantViewHeight = self.view.frame.height
+            let heightRatio = 210 / (pararantViewHeight - (pararantViewHeight / 6))
+            let moveHeight = move.y * heightRatio
+            
+            videoImageViewHeightConstraint.constant = 280 - moveHeight
+            
+            // imageViewの横幅の動き 150(最小値)
+            let originalWidth = self.view.frame.width
+            let minimumImageViewTrailingConstant = -(originalWidth - (150 + 12))
+            let constant = originalWidth - move.y
+            
+            if minimumImageViewTrailingConstant > constant {
+                videoImageViewTrailingConstraint.constant = -minimumImageViewTrailingConstant
+                return
+            }
+            
+            if constant < -12 {
+                videoImageViewTrailingConstraint.constant = -constant
+            }
+            
+            
         } else if gesture.state == .ended {
             
             UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: {
                 
-                imageView.transform = .identity
-                self.view.layoutIfNeeded()
-                
+                self.backToIdentityAllViews(imageView: imageView as! UIImageView)
             })
-
-            
-            
         }
         
+    }
+    
+    private func backToIdentityAllViews(imageView: UIImageView) {
+        imageView.transform = .identity
+        self.videoImageViewHeightConstraint.constant = 280
+        self.videoImageViewTrailingConstraint.constant = 0
+        self.videoImageViewLeadingConstraint.constant = 0
+        
+        self.view.layoutIfNeeded()
     }
     
 }
